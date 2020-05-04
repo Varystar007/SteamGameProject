4aaa9ccff2e0c6f43f432fd62740f8a2bc14ec17
Varystar007 <varystar007@gmail.com>
2020-05-04 09:55:04 +0900

    歯車を追加しました

---
+++ SteamGameProject/Assets/Scripts/UI/DownArrowMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownArrowMovement : MonoBehaviour
+{
+    bool is_A = true;
+    bool is_B = false;
+
+    int CountA = 0;
+    int CountB = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CountA == 50)
+        {
+            is_A = false;
+            is_B = true;
+            CountA = 0;
+        }
+
+        if (CountB == 5)
+        {
+            is_A = true;
+            is_B = false;
+            CountB = 0;
+        }
+
+        if (is_A == true)
+        {
+            CountA++;
+        }
+
+        if (is_B == true)
+        {
+            CountB++;
+        }
+
+        if (CountA >= 1)
+        {
+            transform.Translate(0.0f, 0.0025f, 0.0f);
+        }
+
+        if (CountB >= 1)
+        {
+            transform.Translate(0.0f, -0.025f, 0.0f);
+        }
+    }
+}
---
+++ SteamGameProject/Assets/Scripts/UI/TemperatureStatus.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureStatus: MonoBehaviour
+{
+    bool stop;
+    bool weak;
+    bool normal;
+    bool strong;
+
+    int TemperatureCount = 0;
+    float RotationPoint;
+
+    GameObject TextController;
+    TemperatureStrength script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stop = true;
+        weak = false;
+        normal = false;
+        strong = false;
+
+        TextController = GameObject.Find("TextController");
+        script = TextController.GetComponent<TemperatureStrength>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            if (stop == true)
+            {
+                stop = false;
+                weak = true;
+            }
+
+            else if (weak == true)
+            {
+                weak = false;
+                normal = true;
+            }
+
+            else if (normal == true)
+            {
+                normal = false;
+                strong = true;
+            }
+
+            else if (strong == true)
+            {
+                strong = false;
+                stop = true;
+            }
+        }
+
+        if (weak == true)
+        {
+            TemperatureCount++;
+            script.is_weak();
+        }
+
+        if (normal == true)
+        {
+            TemperatureCount = TemperatureCount + 2;
+            script.is_normal();
+        }
+
+        if (strong == true)
+        {
+            TemperatureCount = TemperatureCount + 3;
+            script.is_strong();
+        }
+
+        if (stop == true)
+        {
+            TemperatureCount--;
+            script.is_stop();
+        }
+
+        if (TemperatureCount <= -1)
+        {
+            TemperatureCount = 0;
+        }
+
+        if (TemperatureCount >= 4001)
+        {
+            TemperatureCount = 4000;
+        }
+
+        RotationPoint = 120f - TemperatureCount * 0.06f;
+        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, RotationPoint);
+    }
+}
