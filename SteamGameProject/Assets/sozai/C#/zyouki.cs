bbe3b3b8f3d9bb80995208d439bcad90741d65eb
Morisawa_Yosinobu <morisawa49@gmail.com>
2020-05-06 02:35:03 +0900

    不具合で消えたcsファイルの追加

---
+++ SteamGameProject/Assets/sozai/C#/zyouki.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zyouki : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time <= 1) 
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            rigid.gravityScale = 0;
+        }
+    }
+}
